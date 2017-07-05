254a45f550b7fea523c4102d155f519f0b0da7fe
Scott Mackenzie <mackes13@gmail.com>
2017-07-05 17:50:40 +1200

    get admin

--- Web/app/components/home/home.ts
+++ Web/app/components/home/home.ts
@@ -1,22 +1,15 @@
 import {Component} from '@angular/core';
 import {AuthService} from "../../services/AuthService";
 
+
+
+
+
 @Component({
   selector: 'home',
   styleUrls: ['app/components/home/home.css'],
   template: `
-      <!--<div class="container-fluid">-->
-          <!--<div class="row">-->
-              <!--<div class="col col-md-1"></div>-->
-              <!--<div class="col col-md-10">-->
-                <!--<h1>Home Page coming soon....</h1>-->
-                <!--{{profile?.nickname}}-->
-              <!--</div>-->
-              <!--<div class="col col-md-1">-->
-              <!--</div>-->
-            <!---->
-        <!--</div>-->
-      <!--</div>-->
+      
       <div class="panel panel-default profile-area">
         <div class="panel-heading">
           <h3>Profile</h3>
@@ -25,13 +18,18 @@ import {AuthService} from "../../services/AuthService";
           <img src="{{profile?.picture}}" class="avatar" alt="avatar">
           <div>
             <label><i class="glyphicon glyphicon-user"></i> Nickname</label>
-            <h3 class="nickname">{{ profile?.nickname }}</h3>
-            <h3>{{ profile?.roles}}</h3>
-            <h3>{{profile?.gender}}</h3>
+            <h3 class="nickname">{{ profile?.name}} </h3>
+            
+            <h3 class="nickname">{{ profile ? profile['http://test.com/roles'] : '' }}</h3>
+            
+          
+            
           </div>
-          <pre class="full-profile">{{ profile | json }}</pre>
+          
+          <pre>{{ profile | json }}</pre>
         </div>
       </div>
+      
   `
 })
 export default class HomeComponent {
@@ -45,9 +43,16 @@ export default class HomeComponent {
             this.profile = this.auth.userProfile;
         } else {
             this.auth.getProfile((err, profile) => {
-                this.profile = profile;
+                this.profile     = profile;
             });
         }
+
+
+
+
+
+
+
     }
 
 }
