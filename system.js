4da014f00f2234007870a7d5a40199a00b386ae7
Scott Mackenzie <mackes13@gmail.com>
2017-08-09 16:25:12 +1200

    package json fix ups

--- system.js
+++ system.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
+const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -12,5 +11,5 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
