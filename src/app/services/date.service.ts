8515e146dfd75f259acdea8c6f6943f14c5b9c86
Scott Mackenzie <mackes13@gmail.com>
2017-08-11 11:39:04 +1200

    time problems with IE

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -26,27 +26,28 @@ export class DateService {
 
   getMonday(): Date {
     let monday = moment(this.date).startOf('isoweek').format('YYYY, MM, DD');
-    return new Date(monday)
+    return new Date(monday);
   }
 
   getTuesday(): Date {
-    let tuesday = moment(this.date).startOf('isoweek').add(1, 'days').format('YYYY, MM, DD');
-    return new Date(tuesday)
+    let tuesday = moment(this.date).startOf('isoweek').add(1, 'days').format('YYYY-MM-DD');
+    return new Date(tuesday);
   }
 
   getWednesday(): Date {
-    let wednesday = moment(this.date).startOf('isoweek').add(2, 'days').format('YYYY, MM, DD');
-    return new Date(wednesday)
+    let wednesday = moment(this.date).startOf('isoweek').add(2, 'days').format('YYYY-MM-DD');
+    return new Date(wednesday);
   }
 
   getThursday() {
-    let thursday = moment(this.date).startOf('isoweek').add(3, 'days').format('YYYY, MM, DD');
-    return new Date(thursday)
+    let thursday = moment(this.date).startOf('isoweek').add(3, 'days').format('YYYY-MM-DD');
+    return new Date(thursday);
   }
 
   getFriday() {
-    let friday = moment(this.date).startOf('isoweek').add(4, 'days').format('YYYY, MM, DD');
-    return new Date(friday)
+    let friday = moment(this.date).startOf('isoweek').add(4, 'days').format('YYYY-MM-DD');
+    console.log(friday);
+    return new Date(friday);
   }
 
 }
