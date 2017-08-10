27a9d5b06d308e5533f6852e0f4341591c838e42
Scott Mackenzie <mackes13@gmail.com>
2017-08-11 09:55:10 +1200

    IE support

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -23,7 +23,8 @@ import 'zone.js/dist/zone';
 // Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
 
 // Needed for: IE9
-import 'classlist.js';
+// import 'classlist.js';
+import 'classlist/classlist.js'
 
 // Animations
 // Needed for: All but Chrome and Firefox, Not supported in IE9
